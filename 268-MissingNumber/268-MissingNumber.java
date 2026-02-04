b820ad157047ca2f7c0e25cf46dcada646f6dfa7
divyasri242006 <24149012@drngpit.ac.in>
2026-02-04 13:21:09 +0530

    Create 268-MissingNumber

---
+++ 268-MissingNumber/268-MissingNumber.java
@@ -0,0 +1,14 @@
+// Last updated: 2/4/2026, 1:21:08 PM
+import java.util.Arrays;
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i) {
+                return i;
+            }
+        }
+        return nums.length;
+    }
+}
